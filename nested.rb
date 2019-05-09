85bd01b7dd4f09b6fbca6e49443579c850fad22b
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 20:52:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-       hash = programmer_hash[:dennis_ritchie]
+  hash = programmer_hash[:dennis_ritchie]
   return hash
 end
 
