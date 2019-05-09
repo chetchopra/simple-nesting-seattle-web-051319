0c63748cf1a72504d730b7b9b0afcf9411c59057
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 21:00:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -71,10 +71,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		  :yukihiro_matsumoto => {
-  :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-}
+ 		    :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        }
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -89,7 +89,7 @@ def adding_matz
         }
      }
      
-  programmer_hash[:yukihiro_matsumoto] => 5
+
   return programmer_hash
 
     
