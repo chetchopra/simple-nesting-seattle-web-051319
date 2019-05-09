2d71597001df22064266cfb19b9f3f0e83ce8226
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 21:03:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,8 +111,6 @@ def changing_alan
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
      return programmer_hash
-     
-     
 end
 
 def adding_to_dennis
@@ -131,5 +129,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
 end
